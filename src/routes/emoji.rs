538cd5c4447547006b0ef50c6b60026faa2ee1b7
Sebastian <Sebastian@Strobl.net>
2021-03-18 16:34:31 +0100

    Add: emoji routes

--- src/routes/emoji.rs
+++ src/routes/emoji.rs
@@ -1,10 +1,15 @@
-use crate::model::Emoji;
+use crate::model::{Emoji, NewEmoji};
 use actix_web::web::ServiceConfig;
 use actix_web::{delete, get, patch, post, web, Error, HttpRequest, HttpResponse, Responder};
 use sqlx::PgPool;
 
 pub fn endpoints(config: &mut ServiceConfig) {
-    config.service(all);
+    config
+        .service(all)
+        .service(by_id)
+        .service(new)
+        .service(update)
+        .service(delete);
 }
 
 #[get("/api/emojis")]
@@ -15,4 +20,27 @@ pub async fn all(pool: web::Data<PgPool>, request: HttpRequest) -> Result<HttpRe
     } else {
         unimplemented!()
     }
+}
+
+#[get("/api/emojis/{id}")]
+pub async fn by_id(pool: web::Data<PgPool>, web::Path(id): web::Path<i32>) -> Result<HttpResponse, Error> {
+    let item = Emoji::by_id(id, &pool).await.unwrap();
+    Ok(HttpResponse::Ok().json(item))
+}
+
+#[post("/api/emojis")]
+pub async fn new(pool: web::Data<PgPool>, web::Json(item): web::Json<NewEmoji>) -> Result<HttpResponse, Error> {
+    let item = Emoji::create(item, &pool).await.unwrap();
+    Ok(HttpResponse::Ok().json(item))
+}
+
+#[patch("/api/emojis/{id}")]
+pub async fn update(pool: web::Data<PgPool>, web::Path(id): web::Path<i32>, web::Json(item): web::Json<NewEmoji>) -> Result<HttpResponse, Error> {
+    let item = Emoji::update(id, item, &pool).await.unwrap();
+    Ok(HttpResponse::Ok().json(item))
+}
+#[delete("/api/emojis/{id}")]
+pub async fn delete(pool: web::Data<PgPool>, web::Path(id): web::Path<i32>,) -> Result<HttpResponse, Error> {
+    let item = Emoji::delete(id, &pool).await.unwrap();
+    Ok(HttpResponse::Ok().json(item))
 }
